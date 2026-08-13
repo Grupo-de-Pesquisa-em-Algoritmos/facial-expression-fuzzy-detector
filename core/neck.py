3cc7c0b361e0fc40a56c3ec014a28913db181252
VYR4L <fkz.zanatt@gmail.com>
2026-08-13 09:54:53 -0300

    feat(neck): replace upsampling with F.interpolate for improved performance

--- core/neck.py
+++ core/neck.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from torch.nn import functional as F
 from blocks.conv import ConvBlock
 from blocks.c3k2 import C3K2Block
 from blocks.c2psa import C2PSABlock
@@ -19,17 +20,15 @@ class YOLOv11Neck(nn.Module):
     Saídas:
         - N3, N4, N5: features refinadas para o head
     """
-    def __init__(self, channels=[512, 1024, 1024]):
+    def __init__(self, channels=(256, 512, 1024)):
         super().__init__()
         c3, c4, c5 = channels
         
         # Top-down pathway (de P5 para P3)
         self.reduce_c5 = ConvBlock(c5, c4, kernel_size=1)
-        self.upsample1 = nn.Upsample(scale_factor=2, mode='nearest')
         self.c3k2_td1 = C3K2Block(c4 + c4, c4, bottleneck_channels=1, shortcut=False)
         
         self.reduce_c4 = ConvBlock(c4, c3, kernel_size=1)
-        self.upsample2 = nn.Upsample(scale_factor=2, mode='nearest')
         self.c3k2_td2 = C3K2Block(c3 + c3, c3, bottleneck_channels=1, shortcut=False)
         
         # Bottom-up pathway (de P3 para P5)
@@ -59,25 +58,27 @@ class YOLOv11Neck(nn.Module):
         # Top-down pathway
         # P5 -> P4
         x = self.reduce_c5(p5)
-        x = self.upsample1(x)
+        x = F.interpolate(x, size=p4.shape[-2:], mode='nearest')
         x = torch.cat([x, p4], dim=1)
         x = self.c3k2_td1(x)  # features intermediárias em escala P4
         p4_td = x
         
         # P4 -> P3
         x = self.reduce_c4(x)
-        x = self.upsample2(x)
+        x = F.interpolate(x, size=p3.shape[-2:], mode='nearest')
         x = torch.cat([x, p3], dim=1)
         n3 = self.c3k2_td2(x)  # N3 - features de escala pequena
         
         # Bottom-up pathway
         # P3 -> P4
         x = self.downsample1(n3)
+        x = F.interpolate(x, size=p4_td.shape[-2:], mode='nearest')
         x = torch.cat([x, p4_td], dim=1)
         n4 = self.c3k2_bu1(x)  # N4 - features de escala média
         
         # P4 -> P5
         x = self.downsample2(n4)
+        x = F.interpolate(x, size=p5.shape[-2:], mode='nearest')
         x = torch.cat([x, p5], dim=1)
         n5 = self.c3k2_bu2(x)  # N5 - features de escala grande
         
